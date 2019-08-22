dfb6728f183cce2285ee7d9f4b7f0ac8cf9f7c7c
Michael Levesque <michaelltd@mac.com>
2019-08-21 23:16:08 -0700

    Added tracking data model.

--- CameraTracker3DSMaxPlugin/Model/TrackingData.cs
+++ CameraTracker3DSMaxPlugin/Model/TrackingData.cs
@@ -1,14 +1,171 @@
-﻿
+﻿using System.Collections.Generic;
+using CameraTracker3DSMaxPlugin.Utilities;
+
 namespace CameraTracker3DSMaxPlugin.Model {
 
-    public struct DataEntry {
-        double timeStamp;
-        Point3 position;
-        Point3 rotation;
+    public enum InterpolationMethod {
+        NearestNeighbor,
+        Linear,
+        Cubic
+    }
+
+    public class TrackingEntry {
+        public TrackingEntry(double timeStamp, Point3 position, Point3 rotation) {
+            TimeStamp = timeStamp;
+            Position = position;
+            Rotation = rotation;
+        }
+
+        public double TimeStamp;
+        public Point3 Position;
+        public Point3 Rotation;
     }
 
     public class TrackingData {
-        public TrackingData() {
+        private IList<TrackingEntry> m_entries;
+        private double m_duration;
+
+        public bool IsEmpty { get { return m_entries.Count == 0; } }
+        public double Duration { get { return m_duration; } }
+        public int NumberOfEntries { get { return m_entries.Count; } }
+
+        public TrackingData(int capacity = 100) {
+            m_entries = new List<TrackingEntry>(capacity);
+            m_duration = 0.0;
+        }
+
+        public void PushEntry(double deltaTime, Point3 position, Point3 rotation) {
+            // add entry
+            TrackingEntry entry = new TrackingEntry(
+                m_duration,
+                position,
+                rotation
+                );
+            m_entries.Add(entry);
+
+            // update duration
+            m_duration += deltaTime;
+        }
+
+        public TrackingEntry GetEntryAtIndex(int index) {
+            TrackingEntry entry = m_entries[index];
+            return new TrackingEntry(entry.TimeStamp, entry.Position, entry.Rotation);
+        }
+
+        public TrackingEntry GetDataAtTime(double time, InterpolationMethod interpolationMethod = InterpolationMethod.NearestNeighbor) {
+            if (IsEmpty) {
+                return null;
+            }
+            switch (interpolationMethod) {
+                case InterpolationMethod.Linear:
+                    return GetDataLinearAtTime(time);
+                case InterpolationMethod.Cubic:
+                    return GetDataCubicAtTime(time);
+                case InterpolationMethod.NearestNeighbor:
+                default:
+                    return GetDataNearestNeighborAtTime(time);
+            }
+        }
+
+
+        private int FindEntryAtTimeRec(double time, int start, int end) {
+            // base case - we've narrowed down the range
+            if (end - start <= 1) {
+                return start;
+            }
+
+            // get middle index
+            int middle = start + (end - start) / 2;
+            TrackingEntry entry = m_entries[middle];
+
+            // which way should we go?
+            if (time == entry.TimeStamp) {
+                return middle;
+            }
+            else if (time < entry.TimeStamp) {
+                return FindEntryAtTimeRec(time, start, middle);
+            }
+            else {
+                return FindEntryAtTimeRec(time, middle, end);
+            }
+        }
+
+        private int[] GetIndexPairAtTime(double time) {
+            // special case for single entry
+            if (m_entries.Count == 1) {
+                return new int[] { 0, 0 };
+            }
+
+            // find potential starting index for pair
+            int firstIndex = FindEntryAtTimeRec(time, 0, m_entries.Count);
+
+            // if first index is at the end, then shift backwards
+            if (firstIndex == m_entries.Count - 1) {
+                return new int[] { firstIndex - 1, firstIndex };
+            }
+            else {
+                return new int[] { firstIndex, firstIndex + 1 };
+            }
+        }
+
+        private int[] GetIndexQuadAtTime(double time) {
+            // get middle pair
+            int[] pair = GetIndexPairAtTime(time);
+
+            // get ending indices
+            int p0 = pair[0] == 0 ? 0 : pair[0] - 1;
+            int p3 = pair[1] == (m_entries.Count - 1) ? pair[1] : pair[1] + 1;
+
+            // return quad
+            return new int[] { p0, pair[0], pair[1], p3 };
+        }
+
+        private float NormalizeTimeAtRange(double time, double start, double end) {
+            double diff = end - start;
+            if (diff == 0.0) {
+                return 0.0f;
+            }
+            else {
+                return (float)((time - start) / diff);
+            }
+        }
+
+        private TrackingEntry GetDataNearestNeighborAtTime(double time) {
+            int[] pair = GetIndexPairAtTime(time);
+            TrackingEntry entry1 = m_entries[pair[0]];
+            TrackingEntry entry2 = m_entries[pair[1]];
+            float t = NormalizeTimeAtRange(time, entry1.TimeStamp, entry2.TimeStamp);
+            return new TrackingEntry(
+                time,
+                Interpolations.NearestNeighbor(entry1.Position, entry2.Position, t),
+                Interpolations.NearestNeighbor(entry1.Rotation, entry2.Rotation, t)
+                );
+        }
+
+        private TrackingEntry GetDataLinearAtTime(double time) {
+            int[] pair = GetIndexPairAtTime(time);
+            TrackingEntry entry1 = m_entries[pair[0]];
+            TrackingEntry entry2 = m_entries[pair[1]];
+            float t = NormalizeTimeAtRange(time, entry1.TimeStamp, entry2.TimeStamp);
+            return new TrackingEntry(
+                time,
+                Interpolations.Linear(entry1.Position, entry2.Position, t),
+                Interpolations.Linear(entry1.Rotation, entry2.Rotation, t)
+                );
+        }
+
+        private TrackingEntry GetDataCubicAtTime(double time) {
+            int[] quad = GetIndexQuadAtTime(time);
+            TrackingEntry entry1 = m_entries[quad[0]];
+            TrackingEntry entry2 = m_entries[quad[1]];
+            TrackingEntry entry3 = m_entries[quad[2]];
+            TrackingEntry entry4 = m_entries[quad[3]];
+            float t = NormalizeTimeAtRange(time, entry2.TimeStamp, entry3.TimeStamp);
+            return new TrackingEntry(
+                time,
+                Interpolations.Cubic(entry1.Position, entry2.Position, entry3.Position, entry4.Position, t),
+                Interpolations.Cubic(entry1.Rotation, entry2.Rotation, entry3.Rotation, entry4.Rotation, t)
+                );
         }
     }
 }
---
+++ CameraTracker3DSMaxPluginTest/Model/TrackingDataTest.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CameraTracker3DSMaxPlugin.Model;
+
+namespace CameraTracker3DSMaxPluginTest.Model {
+    [TestClass]
+    public class TrackingDataTest {
+        void PerformEntryComparison(TrackingEntry expected, TrackingEntry actual) {
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expected.TimeStamp, actual.TimeStamp, 0.000001);
+            Assert.AreEqual<Point3>(expected.Position, actual.Position);
+            Assert.AreEqual<Point3>(expected.Rotation, actual.Rotation);
+        }
+
+        [TestMethod]
+        public void TestIsEmpty() {
+            TrackingData emptyData = new TrackingData(0);
+            TrackingData nonEmptyData = new TrackingData(1);
+            nonEmptyData.PushEntry(1.0, new Point3(), new Point3());
+            Assert.IsTrue(emptyData.IsEmpty);
+            Assert.IsFalse(nonEmptyData.IsEmpty);
+        }
+
+        [TestMethod]
+        public void TestDuration() {
+            TrackingData emptyData = new TrackingData(0);
+            TrackingData nonEmptyData = new TrackingData(4);
+            double[] durations = { 1.0, 2.3, 0.4, 0.8 };
+            double total = 0.0;
+            foreach (double d in durations) {
+                nonEmptyData.PushEntry(d, new Point3(), new Point3());
+                total += d;
+            }
+            Assert.AreEqual(0.0, emptyData.Duration);
+            Assert.AreEqual(total, nonEmptyData.Duration);
+        }
+
+        [TestMethod]
+        public void TestEmptyDataInterpolation() {
+            TrackingData emptyData = new TrackingData(0);
+            Assert.IsNull(emptyData.GetDataAtTime(0.0));
+        }
+
+        [TestMethod]
+        public void TestNearestNeighborInterpolationSingleEntry() {
+            TrackingData data = new TrackingData(1);
+            TrackingEntry expected = new TrackingEntry(0.0, new Point3(0.0f, 1.0f, 2.0f), new Point3(3.0f, 4.0f, 5.0f));
+            data.PushEntry(1.0, expected.Position, expected.Rotation);
+            // t = 0.0
+            expected.TimeStamp = 0.0;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.0, InterpolationMethod.NearestNeighbor));
+            // t = 0.25
+            expected.TimeStamp = 0.25;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.25, InterpolationMethod.NearestNeighbor));
+            // t = 0.5
+            expected.TimeStamp = 0.5;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.5, InterpolationMethod.NearestNeighbor));
+            // t = 0.75
+            expected.TimeStamp = 0.75;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.75, InterpolationMethod.NearestNeighbor));
+            // t = 1.0
+            expected.TimeStamp = 1.0;
+            PerformEntryComparison(expected, data.GetDataAtTime(1.0, InterpolationMethod.NearestNeighbor));
+        }
+
+        [TestMethod]
+        public void TestNearestNeighborInterpolationDoubleEntry() {
+            TrackingData data = new TrackingData(2);
+            TrackingEntry expected1 = new TrackingEntry(0.0, new Point3(0.0f, 1.0f, 2.0f), new Point3(3.0f, 4.0f, 5.0f));
+            TrackingEntry expected2 = new TrackingEntry(0.0, new Point3(6.0f, 7.0f, 8.0f), new Point3(4.0f, 3.0f, 2.0f));
+            data.PushEntry(2.0, expected1.Position, expected1.Rotation);
+            data.PushEntry(1.0, expected2.Position, expected2.Rotation);
+            // t = 0.0
+            expected1.TimeStamp = 0.0;
+            PerformEntryComparison(expected1, data.GetDataAtTime(expected1.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 0.5
+            expected1.TimeStamp = 0.5;
+            PerformEntryComparison(expected1, data.GetDataAtTime(expected1.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 1.0
+            expected2.TimeStamp = 1.0;
+            PerformEntryComparison(expected2, data.GetDataAtTime(expected2.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 1.5
+            expected2.TimeStamp = 1.5;
+            PerformEntryComparison(expected2, data.GetDataAtTime(expected2.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 2.0
+            expected2.TimeStamp = 2.0;
+            PerformEntryComparison(expected2, data.GetDataAtTime(expected2.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 3.0
+            expected2.TimeStamp = 3.0;
+            PerformEntryComparison(expected2, data.GetDataAtTime(expected2.TimeStamp, InterpolationMethod.NearestNeighbor));
+        }
+
+        [TestMethod]
+        public void TestNearestNeighborInterpolationMultipleEntry() {
+            TrackingData data = new TrackingData(5);
+            TrackingEntry expected1 = new TrackingEntry(0.0, new Point3(0.0f, 1.0f, 2.0f), new Point3(3.0f, 4.0f, 5.0f));
+            TrackingEntry expected2 = new TrackingEntry(0.0, new Point3(6.0f, 7.0f, 8.0f), new Point3(4.0f, 3.0f, 2.0f));
+            TrackingEntry expected3 = new TrackingEntry(0.0, new Point3(1.0f, 1.0f, 6.0f), new Point3(1.0f, 2.0f, 8.0f));
+            TrackingEntry expected4 = new TrackingEntry(0.0, new Point3(3.0f, 2.0f, 3.0f), new Point3(0.0f, 1.0f, 6.0f));
+            TrackingEntry expected5 = new TrackingEntry(0.0, new Point3(9.0f, 5.0f, 1.0f), new Point3(6.0f, 0.0f, 1.0f));
+            data.PushEntry(1.0, expected1.Position, expected1.Rotation);
+            data.PushEntry(6.0, expected2.Position, expected2.Rotation);
+            data.PushEntry(3.0, expected3.Position, expected3.Rotation);
+            data.PushEntry(5.0, expected4.Position, expected4.Rotation);
+            data.PushEntry(1.0, expected5.Position, expected5.Rotation);
+            // t = 0.0
+            expected1.TimeStamp = 0.0;
+            PerformEntryComparison(expected1, data.GetDataAtTime(expected1.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 2.0
+            expected2.TimeStamp = 2.0;
+            PerformEntryComparison(expected2, data.GetDataAtTime(expected2.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 3.0
+            expected2.TimeStamp = 3.0;
+            PerformEntryComparison(expected2, data.GetDataAtTime(expected2.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 8.0
+            expected3.TimeStamp = 8.0;
+            PerformEntryComparison(expected3, data.GetDataAtTime(expected3.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 8.5
+            expected4.TimeStamp = 8.5;
+            PerformEntryComparison(expected4, data.GetDataAtTime(expected4.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 12.0
+            expected4.TimeStamp = 12.0;
+            PerformEntryComparison(expected4, data.GetDataAtTime(expected4.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 14.0
+            expected5.TimeStamp = 14.0;
+            PerformEntryComparison(expected5, data.GetDataAtTime(expected5.TimeStamp, InterpolationMethod.NearestNeighbor));
+            // t = 17.0
+            expected5.TimeStamp = 17.0;
+            PerformEntryComparison(expected5, data.GetDataAtTime(expected5.TimeStamp, InterpolationMethod.NearestNeighbor));
+        }
+
+        [TestMethod]
+        public void TestLinearInterpolationSingleEntry() {
+            TrackingData data = new TrackingData(1);
+            TrackingEntry expected = new TrackingEntry(0.0, new Point3(0.0f, 1.0f, 2.0f), new Point3(3.0f, 4.0f, 5.0f));
+            data.PushEntry(1.0, expected.Position, expected.Rotation);
+            // t = 0.0
+            expected.TimeStamp = 0.0;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.0, InterpolationMethod.Linear));
+            // t = 0.25
+            expected.TimeStamp = 0.25;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.25, InterpolationMethod.Linear));
+            // t = 0.5
+            expected.TimeStamp = 0.5;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.5, InterpolationMethod.Linear));
+            // t = 0.75
+            expected.TimeStamp = 0.75;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.75, InterpolationMethod.Linear));
+            // t = 1.0
+            expected.TimeStamp = 1.0;
+            PerformEntryComparison(expected, data.GetDataAtTime(1.0, InterpolationMethod.Linear));
+        }
+
+        [TestMethod]
+        public void TestLinearInterpolationDoubleEntry() {
+            TrackingData data = new TrackingData(2);
+            TrackingEntry expected;
+            data.PushEntry(2.0, new Point3(0.0f, 0.0f, 1.0f), new Point3(0.0f, 0.0f, 1.0f));
+            data.PushEntry(1.0, new Point3(1.0f, -2.0f, 1.0f), new Point3(1.0f, -2.0f, 1.0f));
+            // t = 0.0
+            expected = new TrackingEntry(0.0, new Point3(0.0f, 0.0f, 1.0f), new Point3(0.0f, 0.0f, 1.0f));
+            PerformEntryComparison(expected, data.GetDataAtTime(expected.TimeStamp, InterpolationMethod.Linear));
+            // t = 0.5
+            expected = new TrackingEntry(0.5, new Point3(0.25f, -0.5f, 1.0f), new Point3(0.25f, -0.5f, 1.0f));
+            PerformEntryComparison(expected, data.GetDataAtTime(expected.TimeStamp, InterpolationMethod.Linear));
+            // t = 1.0
+            expected = new TrackingEntry(1.0, new Point3(0.5f, -1.0f, 1.0f), new Point3(0.5f, -1.0f, 1.0f));
+            PerformEntryComparison(expected, data.GetDataAtTime(expected.TimeStamp, InterpolationMethod.Linear));
+            // t = 1.5
+            expected = new TrackingEntry(1.5, new Point3(0.75f, -1.5f, 1.0f), new Point3(0.75f, -1.5f, 1.0f));
+            PerformEntryComparison(expected, data.GetDataAtTime(expected.TimeStamp, InterpolationMethod.Linear));
+            // t = 2.0
+            expected = new TrackingEntry(2.0, new Point3(1.0f, -2.0f, 1.0f), new Point3(1.0f, -2.0f, 1.0f));
+            PerformEntryComparison(expected, data.GetDataAtTime(expected.TimeStamp, InterpolationMethod.Linear));
+        }
+
+        [TestMethod]
+        public void TestCubicInterpolationSingleEntry() {
+            TrackingData data = new TrackingData(1);
+            TrackingEntry expected = new TrackingEntry(0.0, new Point3(0.0f, 1.0f, 2.0f), new Point3(3.0f, 4.0f, 5.0f));
+            data.PushEntry(1.0, expected.Position, expected.Rotation);
+            // t = 0.0
+            expected.TimeStamp = 0.0;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.0, InterpolationMethod.Cubic));
+            // t = 0.25
+            expected.TimeStamp = 0.25;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.25, InterpolationMethod.Cubic));
+            // t = 0.5
+            expected.TimeStamp = 0.5;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.5, InterpolationMethod.Cubic));
+            // t = 0.75
+            expected.TimeStamp = 0.75;
+            PerformEntryComparison(expected, data.GetDataAtTime(0.75, InterpolationMethod.Cubic));
+            // t = 1.0
+            expected.TimeStamp = 1.0;
+            PerformEntryComparison(expected, data.GetDataAtTime(1.0, InterpolationMethod.Cubic));
+        }
+    }
+}
